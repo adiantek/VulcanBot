6e33f12e0ec24ec2c9d300cf51752236acda22b1
adiantek <adiantek@gmail.com>
2018-11-19 10:38:03 +0100

    Vulcan certificate

---
+++ src/main/java/ovh/adiantek/app/vulcanbot/Certificate.java
@@ -0,0 +1,14 @@
+package ovh.adiantek.app.vulcanbot;
+
+public class Certificate {
+	public String CertyfikatKlucz;
+	public String CertyfikatKluczSformatowanyTekst;
+	public Long CertyfikatDataUtworzenia;
+	public String CertyfikatDataUtworzeniaSformatowanyTekst;
+	public String CertyfikatPfx;
+	public String GrupaKlientow;
+	public String AdresBazowyRestApi;
+	public String UzytkownikLogin;
+	public String UzytkownikNazwa;
+	public String TypKonta;
+}
---
+++ src/main/java/ovh/adiantek/app/vulcanbot/VulcanBot.java
@@ -0,0 +1,9 @@
+package ovh.adiantek.app.vulcanbot;
+
+public class VulcanBot {
+
+	public static void main(String[] args) {
+		System.out.println("asdf");
+	}
+
+}
